829a01ae38a782c4b72e2740906109fefcb7aa5f
Simon E. Silva Lauinger <simon@silvalauinger.de>
2015-11-19 15:08:12 +0100

    Implemented draft of the SearchView.

---
+++ src/main/java/de/nanobyte/lighttagfx/ui/search/SearchView.java
@@ -0,0 +1,71 @@
+package de.nanobyte.lighttagfx.ui.search;
+
+import de.nanobyte.javafx.util.LambdaStringConverter;
+import de.saxsys.mvvmfx.InjectViewModel;
+import de.saxsys.mvvmfx.JavaView;
+import java.io.File;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.DirectoryChooser;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.controlsfx.control.CheckTreeView;
+import org.controlsfx.tools.Borders;
+import org.fxmisc.easybind.EasyBind;
+
+public class SearchView extends VBox implements JavaView<SearchViewModel>, Initializable {
+
+    @InjectViewModel
+    private SearchViewModel searchViewModel;
+
+    private final TextField searchFolder = new TextField();
+    private final Button chooseSearchFolderButton = new Button("...");
+    private final DirectoryChooser searchFolderChooser = new DirectoryChooser();
+    private final TextField tagFilter = new TextField();
+    private final CheckTreeView<String> tagSelection = new CheckTreeView<>(new CheckBoxTreeItem<>());
+    private final FilteredList<TreeItem<String>> visibleTags = new FilteredList<>(FXCollections
+            .observableArrayList(new CheckBoxTreeItem<>("lol"), new CheckBoxTreeItem<>("frol")));
+
+    private final static double DEFAULT_SPACING = 7;
+
+    public SearchView() {
+        Bindings.bindBidirectional(searchFolder.textProperty(), searchFolderChooser.initialDirectoryProperty(),
+                new LambdaStringConverter<>(file -> Optional.ofNullable(file).map(File::toString).orElse(""),
+                        stringRepresentation -> Optional.ofNullable(new File(stringRepresentation))
+                        .filter(File::isDirectory).orElseGet(SystemUtils::getUserHome)));
+        chooseSearchFolderButton.setOnAction((final ActionEvent actionEvent)
+                -> Optional.ofNullable(searchFolderChooser.showDialog(((Node) actionEvent.getTarget()).getScene()
+                        .getWindow()))
+                .ifPresent(searchFolderChooser::setInitialDirectory));
+        
+        EasyBind.subscribe(tagFilter.textProperty(), (final String newValue) -> {
+            visibleTags.setPredicate(treeItem -> StringUtils.containsIgnoreCase(treeItem.getValue(), newValue));
+        });
+        EasyBind.listBind(tagSelection.getRoot().getChildren(), visibleTags);
+        tagSelection.setShowRoot(false);
+
+        setSpacing(DEFAULT_SPACING);
+        setPadding(new Insets(DEFAULT_SPACING));
+        getChildren().addAll(new HBox(DEFAULT_SPACING, searchFolder, chooseSearchFolderButton));
+        HBox.setHgrow(searchFolder, Priority.ALWAYS);
+        final Node tagSelectionNodes = Borders.wrap(new VBox(DEFAULT_SPACING, tagFilter, tagSelection)).lineBorder()
+                .outerPadding(DEFAULT_SPACING, 0, 0, 0).innerPadding(DEFAULT_SPACING).title("Tags").buildAll();
+        VBox.setVgrow(tagSelectionNodes, Priority.ALWAYS);
+        getChildren().addAll(tagSelectionNodes, new Button("Search for files with selected tags"));
+        VBox.setVgrow(tagSelection, Priority.ALWAYS);
+    }
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+    }
+}
---
+++ src/main/java/de/nanobyte/lighttagfx/ui/search/SearchViewModel.java
@@ -0,0 +1,7 @@
+package de.nanobyte.lighttagfx.ui.search;
+
+import de.saxsys.mvvmfx.ViewModel;
+
+public class SearchViewModel implements ViewModel {
+       
+}
