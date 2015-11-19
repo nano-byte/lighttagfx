bac816c48afa685c17488739666a66772224663e
Simon E. Silva Lauinger <simon@silvalauinger.de>
2015-11-19 15:08:29 +0100

    Showing SearchView on program start.

--- src/main/java/de/nanobyte/lighttagfx/Main.java
+++ src/main/java/de/nanobyte/lighttagfx/Main.java
@@ -1,17 +1,21 @@
 package de.nanobyte.lighttagfx;
 
+import de.nanobyte.lighttagfx.ui.search.SearchView;
+import de.saxsys.mvvmfx.FluentViewLoader;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     public static void main(final String[] rawArguments) {
-        launch(Main.class, rawArguments);
+        Application.launch(Main.class, rawArguments);
     }
-    
+
     @Override
     public void start(final Stage primaryStage) throws Exception {
-        primaryStage.setTitle("LightTagFX");
+        primaryStage.setTitle("LigthTagFX - Search");
+        primaryStage.setScene(new Scene(FluentViewLoader.javaView(SearchView.class).load().getView()));
         primaryStage.show();
     }
 }
